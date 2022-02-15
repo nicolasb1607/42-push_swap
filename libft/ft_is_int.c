41978a336647a012d55e477003086ef76dafaf45
Nicolas Burat <nburat-d@student.42.fr>
2022-02-15 15:04:18 +0100

    ft_is_int corrige avec adrian mais pas a la norme

--- libft/ft_is_int.c
+++ libft/ft_is_int.c
@@ -6,25 +6,43 @@
 /*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 18:58:42 by nburat-d          #+#    #+#             */
-/*   Updated: 2022/02/12 19:22:23 by nburat-d         ###   ########.fr       */
+/*   Updated: 2022/02/15 15:03:13 by nburat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft.h"
 
+void	del_zero(char *str, int i)
+{
+	int	len;
+
+	len = ft_strlen(str);
+	while(str[i] == '0' && str[i + 1] != '\0')
+	{
+		ft_memcpy(&str[i], &str[i + 1], ft_strlen(&str[i + 1]));
+		str[len - 1] = 0;
+		len--;
+	}	
+}
+
 int	ft_is_int(char *str)
 {
 	int	i;
-
+	int len;
 	i = 0;
+	
 	if (str[i] == '-' || str[i] == '+')
 		i++;
-	while (str[i])
+	len = ft_strlen(str);
+	while(str[i] == '0' && str[i + 1] != '\0')
 	{
-		if (ft_isdigit(str[i]) == 0)
+		ft_memcpy(&str[i], &str[i + 1], ft_strlen(&str[i + 1]));
+		str[len - 1] = 0;
+		len--;
+	}	
+	while (str[i])
+		if (ft_isdigit(str[i++]) == 0)
 			return (0);
-		i++;
-	}
 	if (ft_strlen(str) < 10)
 		return (1);
 	if (ft_strlen (str) > 11)
@@ -32,7 +50,7 @@ int	ft_is_int(char *str)
 	if (ft_strlen (str) == 10)
 		if (ft_strcmp("2147483647", str) < 0)
 			return (0);
-	if (ft_strlen(str) == 11)
+	if (ft_strlen(str)  == 11)
 		if (ft_strcmp("-2147483648", str) < 0)
 			return (0);
 	return (1);
